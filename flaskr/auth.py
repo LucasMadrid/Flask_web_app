80c773b81eb94f43d4fab64bbd5883a6d9bd582f
LucasMadrid <madridlucas75@gmail.com>
2021-07-16 19:52:31 -0300

    Login html added and logic to signup for user validation

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
@@ -10,6 +10,23 @@ def login():
 def logout():
     return render_template('home.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',  methods=['GET','POST'])
 def sign_up():
-    return render_template('signup.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password = request.form.get('password1') 
+        confirm_password = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) <2:
+            pass
+        elif password != confirm_password:
+            pass
+        else:
+            pass
+        
+        return render_template('signup.html', badPassword=False)
+    else:
+        return render_template('signup.html', badPassword=False)
